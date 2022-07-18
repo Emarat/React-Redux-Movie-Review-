ed6ebb144ebdd4723bedec157985b0fa1bd49b5a
Emarat <emarathossain00@gmail.com>
2022-07-19 04:17:08 +0600

    movie card

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAllMovies } from '../../features/movies/movieSlice';
+import MovieCart from "../MovieCart/MovieCart";
+import './MovieListing.scss';
 
 const MovieListing = () => {
+    const movies = useSelector(getAllMovies);
+    let renderMovies = "";
+
+    renderMovies =
+        movies.Response === "True" ? (
+            movies.Search.map((movie, index) => (
+                <MovieCart key={index} data={movie} />
+            ))
+        ) : (
+            <div className="movies-error">
+                <h3>{movies.Error}</h3>
+            </div>
+        );
     return (
-        <div>
-            Movie Listing
+        <div className='movie-wrapper'>
+            <div className='movie-list'>
+                <h2>Movies</h2>
+                <div className='movie-container'>
+                    {renderMovies}
+                </div>
+            </div>
         </div>
     );
 };
